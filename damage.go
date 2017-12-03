1bbe08c766e21160633f98d8d392ffcbbceed9fd
crhntr <hunter.christopher@icloud.com>
2017-12-03 15:38:09 -0800

    Added Damage Type

---
+++ damage.go
@@ -0,0 +1,32 @@
+package pacmound
+
+import "fmt"
+
+type Damage float64
+
+const (
+	DamageAgeing    Damage = 0.05
+	DamageLostFight Damage = 500.0
+	DamageColision  Damage = 0.1
+)
+
+func (d Damage) Error() string {
+	switch d {
+	case DamageAgeing:
+		return "Damage due to ageing"
+	case DamageLostFight:
+		return "Damage due to fight lost"
+	case DamageColision:
+		return "Damage due to colision"
+	default:
+		return fmt.Sprintf("unknown damage %f", d)
+	}
+}
+
+func (ad *AgentData) Damage(d Damage) {
+	ad.score -= float64(d)
+	ad.a.Damage(d)
+	if ad.dead = ad.score <= 0; ad.dead {
+		ad.a.Kill()
+	}
+}
